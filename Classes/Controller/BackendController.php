7dfb3ab2f74ca89aa6ecfea6aeac5b7df1fb72ed
Samuel Mihal <mihal.samuel431@gmail.com>
2020-07-15 17:14:24 +0200

    [TASK] Remove dependencies on DB

--- Classes/Controller/BackendController.php
+++ Classes/Controller/BackendController.php
@@ -1,7 +1,6 @@
 <?php
 namespace Digitalwerk\DwLogNotifier\Controller;
 
-use Digitalwerk\DwLogNotifier\Domain\Model\Main;
 use TYPO3\CMS\Backend\View\BackendTemplateView;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -21,12 +20,6 @@ class BackendController extends ActionController
      * @var string
      */
     protected $defaultViewObjectName = \TYPO3\CMS\Backend\View\BackendTemplateView::class;
-
-    /**
-     * @var \Digitalwerk\DwLogNotifier\Domain\Repository\MainRepository
-     * @Inject()
-     */
-    protected $mainRepository = null;
 
     /**
      * Set up the doc header properly here
@@ -51,119 +44,27 @@ class BackendController extends ActionController
      */
 	function indexAction()
 	{
-	    /** @var Main $main */
-        $main = $this->mainRepository->getCurrent();
-        if (empty($main)) {
-	        $main = new Main();
-            $this->mainRepository->add($main);
-            $this->mainRepository->getPersistenceManager()->persistAll();
+	    $keyPostFix = '';
+        if (StringUtility::beginsWith(TYPO3_branch, '8')) {
+            $dwLogNotifierConfiguration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['dw_log_notifier']);
+            $keyPostFix = '.';
+        } else {
+            if (
+                isset($GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['dw_log_notifier'])
+                && is_array($GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['dw_log_notifier'])
+            ) {
+                $dwLogNotifierConfiguration =
+                    GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)
+                        ->get('dw_log_notifier');
+            }
         }
-
-        $this->updateSerializedObject($main);
+        $dwLogNotifierConfiguration = $dwLogNotifierConfiguration['errorLogReporting' . $keyPostFix];
         $this->view->assignMultiple([
-			'main' => $main,
+			'enable' => $dwLogNotifierConfiguration['enabled' . $keyPostFix],
+			'emails' => $dwLogNotifierConfiguration['email' . $keyPostFix]['addresses' . $keyPostFix],
+			'omitExceptions' => $dwLogNotifierConfiguration['omitExceptions' . $keyPostFix],
+			'disabledContext' => $dwLogNotifierConfiguration['disabledTypo3Context' . $keyPostFix],
+			'slack' => $dwLogNotifierConfiguration['slack' . $keyPostFix],
 		]);
 	}
-
-    /**
-     * Status action
-     * @param Main $main
-     * @throws \TYPO3\CMS\Extbase\Mvc\Exception\StopActionException
-     * @throws \TYPO3\CMS\Extbase\Mvc\Exception\UnsupportedRequestTypeException
-     * @throws \TYPO3\CMS\Extbase\Persistence\Exception\IllegalObjectTypeException
-     * @throws \TYPO3\CMS\Extbase\Persistence\Exception\UnknownObjectException
-     */
-    public function statusAction(Main $main)
-    {
-        if ($main->isEnable()) {
-            $main->setEnable(false);
-        } else {
-            $main->setEnable(true);
-        }
-        $this->mainRepository->update($main);
-        $this->mainRepository->getPersistenceManager()->persistAll();
-        $this->redirect('index');
-    }
-
-    /**
-     * Update action
-     * @param Main $main
-     * @throws \TYPO3\CMS\Extbase\Mvc\Exception\StopActionException
-     * @throws \TYPO3\CMS\Extbase\Mvc\Exception\UnsupportedRequestTypeException
-     * @throws \TYPO3\CMS\Extbase\Persistence\Exception\IllegalObjectTypeException
-     * @throws \TYPO3\CMS\Extbase\Persistence\Exception\UnknownObjectException
-     */
-    public function updateAction(Main $main)
-    {
-        $this->mainRepository->update($main);
-        $this->mainRepository->getPersistenceManager()->persistAll();
-        $this->redirect('index');
-    }
-
-    /**
-     * Delete omit exception action
-     * @param Main $main
-     * @param $id
-     * @throws \TYPO3\CMS\Extbase\Mvc\Exception\StopActionException
-     * @throws \TYPO3\CMS\Extbase\Mvc\Exception\UnsupportedRequestTypeException
-     * @throws \TYPO3\CMS\Extbase\Persistence\Exception\IllegalObjectTypeException
-     * @throws \TYPO3\CMS\Extbase\Persistence\Exception\UnknownObjectException
-     */
-    public function deleteOmitExceptionsItemAction(Main $main, int $id)
-    {
-        $omitExceptions = $main->getArrayOfProperty($main->getOmitExceptions());
-        unset($omitExceptions[$id]);
-        $main->setStringOfArrayProperty($omitExceptions, 'omitExceptions');
-        $this->updateAction($main);
-    }
-
-    /**
-     * Delete email action
-     * @param Main $main
-     * @param int $id
-     * @throws \TYPO3\CMS\Extbase\Mvc\Exception\StopActionException
-     * @throws \TYPO3\CMS\Extbase\Mvc\Exception\UnsupportedRequestTypeException
-     * @throws \TYPO3\CMS\Extbase\Persistence\Exception\IllegalObjectTypeException
-     * @throws \TYPO3\CMS\Extbase\Persistence\Exception\UnknownObjectException
-     */
-    public function deleteEmailsItemAction(Main $main, int $id)
-    {
-        $emails = $main->getArrayOfProperty($main->getEmails());
-        unset($emails[$id]);
-        $main->setStringOfArrayProperty($emails, 'emails');
-        $this->updateAction($main);
-    }
-
-    /**
-     * Delete email action
-     * @param Main $main
-     * @param int $id
-     * @throws \TYPO3\CMS\Extbase\Mvc\Exception\StopActionException
-     * @throws \TYPO3\CMS\Extbase\Mvc\Exception\UnsupportedRequestTypeException
-     * @throws \TYPO3\CMS\Extbase\Persistence\Exception\IllegalObjectTypeException
-     * @throws \TYPO3\CMS\Extbase\Persistence\Exception\UnknownObjectException
-     */
-    public function deleteDisabledTypo3ContextItemAction(Main $main, int $id)
-    {
-        $disabledTypo3Context = $main->getArrayOfProperty($main->getDisabledTypo3Context());
-        unset($disabledTypo3Context[$id]);
-        $main->setStringOfArrayProperty($disabledTypo3Context, 'disabledTypo3Context');
-        $this->updateAction($main);
-    }
-
-    /**
-     * @param Main $main
-     */
-    private function updateSerializedObject(Main $main)
-    {
-        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
-            ->getQueryBuilderForTable('tx_dwlognotifier_domain_model_main');
-        $queryBuilder
-            ->update('tx_dwlognotifier_domain_model_main')
-            ->where(
-                $queryBuilder->expr()->eq('uid', $queryBuilder->createNamedParameter($main->getUid()))
-            )
-            ->set('serializable_log_notifier_object', serialize($main))
-            ->execute();
-    }
 }
